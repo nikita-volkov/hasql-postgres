a31f4ab621878c9d4e36f643b28d9ac4eb43e58b
Nikita Volkov <nikita.y.volkov@mail.ru>
2014-11-28 14:21:23 +0300

    Fix the Error conflict by switching to "transformers"

--- library/Hasql/Postgres/Prelude.hs
+++ library/Hasql/Postgres/Prelude.hs
@@ -14,9 +14,12 @@ where
 -------------------------
 import BasePrelude as Exports hiding (assert, left, right)
 
--- mtl-prelude
+-- transformers
 -------------------------
-import MTLPrelude as Exports hiding (shift)
+import Control.Monad.IO.Class as Exports
+import Control.Monad.Trans.Class as Exports
+import Control.Monad.Trans.Reader as Exports hiding (liftCallCC, liftCatch)
+import Control.Monad.Trans.State.Strict as Exports hiding (liftCallCC, liftCatch)
 
 -- either
 -------------------------
